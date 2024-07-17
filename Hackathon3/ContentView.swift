603bd3e9d4335f9eef1a476ba50ffaac5c4ff2a5
Simon Zwicker <github@zwicker.io>
2024-07-17 15:21:45 +0200

    initial

---
+++ Hackathon3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hackathon3
+//
+//  Created by Simon Zwicker on 17.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hackathon3/Hackathon3App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon3App.swift
+//  Hackathon3
+//
+//  Created by Simon Zwicker on 17.07.24.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
