766335b8eb4976b53383de651dadd11d108be8da
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-07-18 08:49:50 -0700

    Added paging for MovieMatch

--- Hackathon3/App/Match/MatchMovieModel.swift
+++ Hackathon3/App/Match/MatchMovieModel.swift
@@ -29,7 +29,7 @@ class MatchMovieModel {
                 let data = try await Network.request(
                     TMDBMovies.self,
                     environment: .tmdb,
-                    endpoint: TmDB.movie("\(genre)")
+                    endpoint: TmDB.movie("\(genre)", page)
                 )
 
                 self.currentMovies = data
